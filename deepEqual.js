cbffc85b417ec649a1db95b1b70bfc5e5cddb4f9
Stewart Morales <stewartm12@github.com>
2021-12-10 15:25:31 -0500

    Finish all algorithm problem

---
+++ deepEqual.js
@@ -0,0 +1,51 @@
+// some examples in python pseudo-code
+// >> equals({"key1" : "value",
+//            "key2" : {"key3": 1}},
+//           {"key1" : "value",
+//            "key2" : {"key3": 1}})
+// >> true
+// >> equals({"key1" : "value1",
+//            "key2" : {"key3": 1},
+//            "key4" : "value2"},
+//           {"key1" : "value",
+//            "key2" : {"key3": 1}})
+// >> false
+
+// Solution 1
+// I decided to compare these two objects using JSON.stringify. This method just mostly cares
+// about the content of each object rather than the checking to see if both objects are contained in the same memory location
+// But I'm probably going to guess this isn't what you are looking for
+const deepEquality = (obj1, obj2) => {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
+};
+
+
+// Solution 2
+// The time complexity here in this solution is pretty tricky. I iterated through an object to grab the keys and values (This is linear time or O(n)).
+// But then we have a conditional here where if the value is an object then, we will call the same function again, also known as recursion.
+// Recursions can be tricky, and I don't have that much experience with the time complexity of a recursion.
+// So i will make an educated guess, the time complexity for this solution is 2^n or n!
+// and the space complexity is constant or O(1).
+const deepEquality2 = (obj1, obj2) => {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+  for (let key in obj1) {
+    if (!obj2.hasOwnProperty(key)) return false;
+
+    if (typeof obj1[key] === 'object') {
+      let isEqual = deepEquality2(obj1[key], obj2[key])
+      if (!isEqual) return false;
+    } else if (obj1[key] !== obj2[key]) {
+      return false
+    };
+  }
+
+  return true;
+}
+
+console.log(
+  deepEquality2(
+    { "key1": "value", "key2": { "key3": 1 } },
+    { "key1": "value", "key2": { "key3": 1 } }
+  )
+);
---
+++ sync.js
@@ -0,0 +1,41 @@
+// some examples in python pseudo-code
+// sync([1, 2, 3], [1, 3, 4])
+// [2, 4]
+// sync([1, 9], [1])
+// [9]
+// sync([1, 2, 3], [1, 2, 3])
+// []
+
+
+// Solution 1
+// I have decided to use a hash to hold all the numbers from both arrays to have the option of grabbing the values in constant time or O(1)
+// I decided to use a for loop to iterate through both input arrays at the same time (giving us a time complexity of linear or O(n))
+// I decided to iterate through the hash (O(n)) to grab any values that contains a value of 1 which presents this value was not present in both lists.
+// At the end, this solution brings us to a time complexity of O(n) (n being the length of the largest array)
+// and a space complexity of O(n + m) (n being the length of arr1 and m being the length of arr2)
+const sync = (arr1, arr2) => {
+  const largeArr = arr1.length > arr2.length ? arr1 : arr2;
+
+  let singleNumbers = [];
+  let syncObject = {};
+
+  for (let i = 0; i < largeArr.length; i++) {
+    let num1 = arr1[i];
+    let num2 = arr2[i];
+
+    if (!syncObject[num1]) syncObject[num1] = 0;
+    if (!syncObject[num2]) syncObject[num2] = 0;
+
+    syncObject[num1] += 1;
+    syncObject[num2] += 1;
+  }
+
+  for (let num in syncObject) {
+    if (syncObject[num] === 1) singleNumbers.push(parseInt(num));
+  }
+
+  return singleNumbers;
+}
+
+console.log(sync([1, 2, 3], [1, 3, 4]));
+
